2c1862de3e1685cd2ba25b2f72ae3c7206cbe1d5
zhengtong0898 <zhengtong0898@aliyun.com>
2021-03-21 00:03:53 +0800

    asyncio: 增加 asyncio.run 方法说明

--- asyncio/asyncio-3.8/runners.py
+++ asyncio/asyncio-3.8/runners.py
@@ -29,17 +29,34 @@ def run(main, *, debug=False):
 
         asyncio.run(main())
     """
+    # run 函数是 asyncio 设计的一个启动入口函数,
+    # 如果之前已经创建过 loop 对象, 抛出异常.
     if events._get_running_loop() is not None:
         raise RuntimeError(
             "asyncio.run() cannot be called from a running event loop")
 
+    # main 必须是一个 coroutine 对象, 否则抛出异常.
     if not coroutines.iscoroutine(main):
         raise ValueError("a coroutine was expected, got {!r}".format(main))
 
+    # 创建一个 loop 对象.
+    # windows: Iocp
+    # linux: epoll
+    # unix: kqueue
     loop = events.new_event_loop()
+
     try:
+        # 将 loop 写入到 DefaultEventLoopPolicy._local._loop
+        # windows: WindowsProactorEventLoopPolicy
+        # linux: UnixDefaultEventLoopPolicy
+        # unix: UnixDefaultEventLoopPolicy
+        #
+        # 其中 _local 的类型是: threading.local(), 用于不同线程之间共享变量.
         events.set_event_loop(loop)
         loop.set_debug(debug)
+
+        # 开始运行 loop 对象(开始执行无限循环: while),
+        # run_until_compelete -> run_forever -> while
         return loop.run_until_complete(main)
     finally:
         try:
